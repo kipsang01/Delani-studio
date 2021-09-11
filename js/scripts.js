9ee25536e6560538cf175f0ebb1d460ae44257aa
kipsang01 <enock.kipsang@student.moringaschool.com>
2021-09-11 11:21:34 +0300

    Add toggle to development and product management

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,15 @@ $(document).ready(function(){
         $(".txt1").toggle()
     })
 
+    $(".development").click(function(){
+        $("#fig2").toggle()
+        $(".txt2").toggle()
+    })
+
+    $(".product-management").click(function(){
+        $("#fig3").toggle()
+        $(".txt3").toggle()
+    })
+
 
 })
