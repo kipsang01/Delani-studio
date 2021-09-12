b7624a6c25417b9d75d4adbadd03e57841fb0c36
kipsang01 <enock.kipsang@student.moringaschool.com>
2021-09-12 09:42:30 +0300

    Add pop up text to contact form

--- js/scripts.js
+++ js/scripts.js
@@ -15,10 +15,10 @@ $(document).ready(function(){
     })
 
     $(".work1").mouseenter(function(){
-        $("#img1").css("background-color" ,"rgba(0, 0, 0, 0.5)")
+        $("#img1,#img2,#img3,#img4,#img5,#img6,#img7,#img8").css("background-color" ,"rgba(0, 0, 0, 0.5)")
         $(".project-name").show();
     })
-    $(".work1").mouseleave(function(){
+    $(".work1,.work2,.work3,.work4,.work4,.work5,.work6,.work7,.work8").mouseleave(function(){
         $("#img1").css({"background":"rgba(0, 0, 0, 0.5);" ,"opacity":"1.0"})
         $(".project-name").hide();
     })
